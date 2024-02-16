63730f25210ecbb49374d41cb795db0c1ed37375
Joseph Agnelli <joeyagnelli@gmail.com>
2024-02-15 17:46:20 -0800

    added sand project (unstarted), home link, and media query for phones

--- scripts.js
+++ scripts.js
@@ -10,4 +10,38 @@ function init() {
 			.open('https://www.linkedin.com/in/joseph-agnelli-b8ba61119/', '_blank')
 			.focus();
 	};
+
+	const showResumeEle = document.getElementById('showResume');
+	let resumeHidden = true;
+
+	function toggleResume() {
+		const resume = document.getElementById('resume');
+
+		if (resumeHidden) {
+			showResumeEle.innerHTML = 'Hide Resume';
+			resume.style.display = 'block';
+		} else {
+			showResumeEle.innerHTML = 'Show Resume';
+			resume.style.display = 'none';
+		}
+		resumeHidden = !resumeHidden;
+	}
+	window.addEventListener('resize', () => {
+		if (resumeHidden && window.innerWidth > 799) {
+			resume.style.display = 'block';
+		} else if (window.innerWidth < 800) {
+			// when shrinking the screen sets the visibility to wherever it was before expanding
+			if (resumeHidden) {
+				showResumeEle.innerHTML = 'Show Resume';
+				resume.style.display = 'none';
+			} else {
+				showResumeEle.innerHTML = 'Hide Resume';
+				resume.style.display = 'block';
+			}
+		}
+	});
+
+	showResumeEle.onclick = () => {
+		toggleResume();
+	};
 }
